9aec125d3c7d17f676bb6b5b8b97c633ba3bda1e
Brian Ketelsen <bketelsen@gmail.com>
2025-10-15 14:17:00 -0400

    feat: site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.linuxsnow.org',
+	site: 'https://linuxsnow.org',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'SNOW Linux',
